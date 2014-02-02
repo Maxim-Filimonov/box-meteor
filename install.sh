9d39f2fff41cde2613a01656236d51def8319823
Sébastien Arnaud <arnaudsj@gmail.com>
2014-02-01 23:01:25 -0500

    Updating to latest versions of meteor, node, phantomjs

--- install.sh
+++ install.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# This is the Meteor install script!
+# Are you looking at this in your web browser, and would like to install Meteor?
+# Just open up your terminal and type:
+#
+#    curl https://install.meteor.com/ | sh
+#
+# Meteor currently supports:
+#   - Mac: OS X 10.6 and above
+#   - Linux: x86 and x86_64 systems
+
+
+
+# Now, on to the actual installer!
+
 ## NOTE sh NOT bash. This script should be POSIX sh only, since we don't
 ## know what shell the user has. Debian uses 'dash' for 'sh', for
 ## example.
@@ -26,7 +40,7 @@ set -u
 # Let's display everything on stderr.
 exec 1>&2
 
-UNAME=`uname`
+UNAME=$(uname)
 if [ "$UNAME" != "Linux" -a "$UNAME" != "Darwin" ] ; then
     echo "Sorry, this OS is not supported yet."
     exit 1
@@ -35,7 +49,7 @@ fi
 
 if [ "$UNAME" = "Darwin" ] ; then
   ### OSX ###
-  if [ "i386" != `uname -p` -o "1" != `sysctl -n hw.cpu64bit_capable 2>/dev/null || echo 0` ] ; then
+  if [ "i386" != "$(uname -p)" -o "1" != "$(sysctl -n hw.cpu64bit_capable 2>/dev/null || echo 0)" ] ; then
     # Can't just test uname -m = x86_64, because Snow Leopard can
     # return other values.
     echo "Only 64-bit Intel processors are supported at this time."
@@ -44,9 +58,9 @@ if [ "$UNAME" = "Darwin" ] ; then
   ARCH="x86_64"
 elif [ "$UNAME" = "Linux" ] ; then
   ### Linux ###
-  ARCH=`uname -m`
+  ARCH=$(uname -m)
   if [ "$ARCH" != "i686" -a "$ARCH" != "x86_64" ] ; then
-    echo "Unable architecture: $ARCH"
+    echo "Unusable architecture: $ARCH"
     echo "Meteor only supports i686 and x86_64 for now."
     exit 1
   fi
@@ -59,14 +73,13 @@ trap "echo Installation failed." EXIT
 # install here:
 [ -e "$HOME/.meteor" ] && rm -rf "$HOME/.meteor"
 
-# This is the CloudFront CDN serving com.meteor.warehouse.
-TARBALL_URL="https://d3fm2vapipm3k9.cloudfront.net/bootstrap/0.6.4/meteor-bootstrap-${PLATFORM}.tar.gz"
+TARBALL_URL="https://warehouse.meteor.com/bootstrap/0.7.0.1/meteor-bootstrap-${PLATFORM}.tar.gz"
 
 INSTALL_TMPDIR="$HOME/.meteor-install-tmp"
 rm -rf "$INSTALL_TMPDIR"
 mkdir "$INSTALL_TMPDIR"
 echo "Downloading Meteor distribution"
-tar -xzvf meteor-bootstrap-Linux_x86_64.tar.gz -C "$INSTALL_TMPDIR"
+curl --progress-bar --fail "$TARBALL_URL" | tar -xzf - -C "$INSTALL_TMPDIR"
 # bomb out if it didn't work, eg no net
 test -x "${INSTALL_TMPDIR}/.meteor/meteor"
 mv "${INSTALL_TMPDIR}/.meteor" "$HOME"
@@ -75,7 +88,7 @@ rmdir "${INSTALL_TMPDIR}"
 test -x "$HOME/.meteor/meteor"
 
 echo
-echo "Meteor 0.6.4 has been installed in your home directory (~/.meteor)."
+echo "Meteor 0.7.0.1 has been installed in your home directory (~/.meteor)."
 
 LAUNCHER="$HOME/.meteor/tools/latest/launch-meteor"
 
@@ -97,6 +110,15 @@ EOF
 elif type sudo >/dev/null 2>&1; then
   echo "Writing a launcher script to $PREFIX/bin/meteor for your convenience."
   echo "This may prompt for your password."
+
+  # New macs (10.9+) don't ship with /usr/local, however it is still in
+  # the default PATH. We still install there, we just need to create the
+  # directory first.
+  if [ ! -d "$PREFIX/bin" ] ; then
+      sudo mkdir -m 755 "$PREFIX" || true
+      sudo mkdir -m 755 "$PREFIX/bin" || true
+  fi
+
   if sudo cp "$LAUNCHER" "$PREFIX/bin/meteor"; then
     cat <<"EOF"
 
